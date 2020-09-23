5bfe7eef31008df59d531d64194d66d0ec7fe43a
HassenH1 <hasansaid51@gmail.com>
2020-09-23 09:38:53 -0700

    mock test

--- __tests__/components/App.test.js
+++ __tests__/components/App.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-import App from "../App";
+import App from "../../App";
 
 describe("<App/>", () => {
   it("App renders without crashing", () => {
     const rendered = renderer.create(<App />).toJSON();
     expect(rendered).toBeTruthy();
+  });
+
+  it("has 1 child", () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(1);
   });
 });
