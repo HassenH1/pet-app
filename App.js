0c28dc127ff19489e6139b35e89fc55f40816346
HassenH1 <hasansaid51@gmail.com>
2020-09-22 18:53:07 -0700

    landing page

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome!!</Text>
+      <View style={styles.imgContainer}>
+        <Image
+          source={{
+            uri:
+              "https://www.pngkit.com/png/full/625-6257944_cute-animal-dogsticker-doglove-freetoedit-transparent-dog-kawaii.png",
+          }}
+          style={styles.img}
+        />
+      </View>
+      <View>
+        <Text>Adopt-a-Pet</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: "space-evenly",
     alignItems: "center",
-    justifyContent: "center",
+    borderColor: "orange",
+    borderWidth: 1,
+    position: "relative",
+  },
+  imgContainer: {
+    width: "73%",
+    position: "absolute",
+    right: -16,
+    top: 80,
+    height: "50%",
+  },
+  img: {
+    width: "100%",
+    // height: 440,
+    height: "100%",
+    // borderColor: "red",
+    // borderWidth: 1,
   },
 });
