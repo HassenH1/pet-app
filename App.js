9600bef8a8b51ced08126a121637fdadb1ff41da
HassenH1 <hasansaid51@gmail.com>
2020-09-22 21:18:32 -0700

    ..

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Button } from "react-native-elements";
+import Login from "./components/login";
 
 export default function App() {
   return (
@@ -46,7 +47,6 @@ export default function App() {
                 title="Login"
                 buttonStyle={styles.btn}
                 onPress={() => console.log("btn has been pressed 1")}
-                // type="outline"
               />
             </View>
             <View>
---
+++ components/login.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Button, Input } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const login = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerConatiner}>
+        <Text style={{ fontSize: 50 }}>Welcome</Text>
+        <Input
+          placeholder="Email"
+          leftIcon={<Icon name="envelope" size={24} color="black" />}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry={true}
+          leftIcon={<Icon name="lock" size={24} color="black" />}
+        />
+
+        <Button
+          title="Login"
+          type="outline"
+          buttonStyle={{ width: "100%" }}
+          onPress={() => console.log("login button has been pressed")}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "whitesmoke",
+    borderRightColor: "orange",
+    borderLeftColor: "orange",
+    borderWidth: 1,
+  },
+  innerConatiner: {
+    height: "80%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
