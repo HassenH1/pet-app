3f7514ab486e6108b5bdb852bcdd948250dd506a
HassenH1 <hasansaid51@gmail.com>
2020-09-22 20:00:26 -0700

    ...still updating style

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { useFonts, CuteFont_400Regular } from "@expo-google-fonts/cute-font";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    CuteFont_400Regular,
+  });
   return (
     <View style={styles.container}>
       <View style={styles.imgContainer}>
@@ -14,8 +18,43 @@ export default function App() {
           style={styles.img}
         />
       </View>
-      <View>
-        <Text>Adopt-a-Pet</Text>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View style={{ borderColor: "orange", borderWidth: 1 }}>
+          <Text style={styles.text}>Adopt-a-Pet</Text>
+        </View>
+
+        <View
+          style={{
+            borderColor: "purple",
+            borderWidth: 1,
+            height: "8rem",
+            width: "100%",
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+              borderColor: "orange",
+              borderWidth: 1,
+            }}
+          >
+            <View>
+              <Button title="Button 1" />
+            </View>
+            <View>
+              <Button title="Button 2" />
+            </View>
+          </View>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -27,8 +66,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "space-evenly",
     alignItems: "center",
-    borderColor: "orange",
-    borderWidth: 1,
     position: "relative",
   },
   imgContainer: {
@@ -40,9 +77,14 @@ const styles = StyleSheet.create({
   },
   img: {
     width: "100%",
-    // height: 440,
     height: "100%",
-    // borderColor: "red",
-    // borderWidth: 1,
+  },
+  textContainer: {
+    zIndex: 1,
+  },
+  text: {
+    fontFamily: "CuteFont_400Regular",
+    fontSize: "80px",
+    color: "#FF4500",
   },
 });
