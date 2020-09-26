6980da8265e3dfa72a6277ad293795b305e56eb4
HassenH1 <hasansaid51@gmail.com>
2020-09-25 21:49:14 -0700

    server skeleton

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+/* 
+server is going to be used to fetch data from restful api 
+then store it into context api for global state
+*/
+
 const express = require("express");
 const app = express();
 const fetch = require("node-fetch");
@@ -5,3 +10,15 @@ const PORT = 8000;
 const methodOverride = require("method-override");
 const cors = require("cors");
 require("dotenv").config();
+
+app.use(cors());
+app.use(express.json()); //does the exact same thing as body-parser
+app.use(methodOverride("_method"));
+
+app.get("/", (req, res) => {
+  res.send("testing server");
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT} port`);
+});
