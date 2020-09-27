5506b7494cfec19779a1b1d7afa441e86b464a1d
HassenH1 <hasansaid51@gmail.com>
2020-09-26 20:43:57 -0700

    ngrok doesnt work right for some reason

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,10 +3,23 @@ import { StyleSheet, Text, View } from "react-native";
 import CardSwipe from "./CardSwipe";
 import { contextAPI } from "../../App";
 import { fetchData } from "../../reducer/fetchData";
+import { url } from "../ngrok/index";
 
 const dashboard = () => {
   const { userState, dispatch } = useContext(contextAPI);
   const { user, loading } = userState;
+
+  const fetchData = async () => {
+    console.log("inside the fetchData function");
+    try {
+      console.log("inside the try catch block");
+      const resp = await fetch(`${url}`);
+      const respJson = await resp.json();
+      console.log(respJson, " <------------------data coming in");
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   useEffect(() => {
     console.log("before fetchData function");
--- src/ngrok/index.js
+++ src/ngrok/index.js
@@ -1,2 +1,2 @@
-const url = "https://c9f3e05485d7.ngrok.io"; //gotta update this before making any fetch calls
+const url = ""; //gotta update this before making any fetch calls
 export default url;
