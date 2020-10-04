b6a443eb6b4452bce6ee9a06df2ee10b8980a19d
HassenH1 <hasansaid51@gmail.com>
2020-10-03 19:26:57 -0700

    testing location..

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,12 +1,15 @@
-import React, { useContext, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import CardSwipe from "./CardSwipe";
 import { useAPI } from "../../context/apiContext";
 import { url } from "../ngrok/index";
+import * as Location from "expo-location";
 
 const Dashboard = () => {
   const { userState, dispatch } = useAPI();
   const { user, loading, data, location } = userState;
+  const [errorMsg, setErrorMsg] = useState();
+  const [loc, setLocation] = useState();
 
   const fetchData = async () => {
     dispatch({ type: "SET_LOADING", payload: true });
@@ -21,6 +24,15 @@ const Dashboard = () => {
   };
 
   useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
     // fetchData();
   }, []);
 
